81fe060771a4c8ea11cf8ff001124d015d0f330e
Justin Smith <11jsmith7@gmail.com>
2015-08-31 09:21:14 -0600

    Initial Commit

---
+++ StoreProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreProject
+//
+//  Created by Justin Smith on 8/31/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreProject
+//
+//  Created by Justin Smith on 8/31/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

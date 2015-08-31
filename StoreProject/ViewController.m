7c1dc5f57de956e9944b2eae636568a556dfd985
Justin Smith <11jsmith7@gmail.com>
2015-08-31 09:53:35 -0600

    Adds iAd to View

--- StoreProject/ViewController.m
+++ StoreProject/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+@import iAd;
 
 @interface ViewController ()
+
+@property (nonatomic, strong) ADBannerView *adBannerView;
 
 @end
 
@@ -17,6 +20,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor colorWithRed:0.149 green:0.604 blue:0.859 alpha:1.000]; //DevMountain Blue
+    
+    self.adBannerView = [[ADBannerView alloc] initWithAdType:ADAdTypeBanner];
+    
+    self.adBannerView.frame = CGRectMake(0, self.view.frame.size.height - 50, self.adBannerView.frame.size.width, self.adBannerView.frame.size.width);
+    
+    [self.view addSubview:self.adBannerView];
 }
 
 - (void)didReceiveMemoryWarning {
